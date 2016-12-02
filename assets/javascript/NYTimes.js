f35d3cb23111b38fc1f653ca930e1fa6f4500494
antoniojgage <antoniojgage@gmail.com>
2016-12-02 16:48:40 -0600

    Searching works, however date with only year fails.

--- assets/javascript/NYTimes.js
+++ assets/javascript/NYTimes.js
@@ -1,42 +1,87 @@
+// Search Term
+// Number of records
+// Start Year
+// End year
+// Top Articles
+
+// $("#search").on("click", function() { //add this back
+//Article Search API: df04ac9d5b75418da22f348bc63aba52
+
+//search
+// endYear
+// startYear
+// numRecordsSelect
+// searchTerm
+$(document).ready(function() {
+
+    function queryData() {
 
 
-    // Search Term
-    // Number of records
-    // Start Year
-    // End year
-    // Top Articles
+        var searchTerm = "'" + $("#searchTerm").val() + "'";
 
-    // $("#search").on("click", function() { //add this back
-    //Article Search API: df04ac9d5b75418da22f348bc63aba52
+        var startYear = $("#startYear").val();
+        console.log(startYear);
+        var endYear = $("#endYear").val();
 
-
-
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({
-        'api-key': "df04ac9d5b75418da22f348bc63aba52",
-        'q': "batman", //search
-        // 'fl': "5", //limit search
-        'begin_date': "20160415", //begin date
-        'end_date': "20161115" //end date
-    });
-         'begin_date': "20160415", //begin date
-        'end_date': "20161115" //end date
-    });
-    $.ajax({
-        url: url,
-        method: 'GET',
-    }).done(function(result) {
-        console.log(result);
-        // var i = 0;
-        for (i = 0; i < result.response.docs.length; i++) {
-            console.log(result.response.docs[i].lead_paragraph);
-            //write to page
-            $("#results").append(result.response.docs[i].lead_paragraph);
-            $("#results").append("<div>" + result.response.docs[i].byline.original + "</div>");
-            $("#results").append("<div></div>");
+        if (startYear === "") {
+            startYear = "20151104";
+            console.log("startYear = null setting to default date " + startYear);
+        } else {
+            startYear = $("#startYear").val();
+            console.log("assigning value to start year " + startYear);
         }
-    }).fail(function(err) {
-        throw err;
+
+        if (endYear === "") {
+            endYear = "20161104";
+            console.log("endYear = null setting to default date " + endYear);
+        } else {
+            endYear = $("#startYear").val();
+            console.log("assigning value to end year " + endYear);
+        }
+
+        var recordsRequested = parseInt($("#numRecordsSelect").val());
+
+        //use this code for next button.
+        // if (recordsRequested = 30) {
+        //     recordsRequested = 2;
+        // } else if (recordsRequested = 20) {
+        //     recordsRequested = 1;
+        // } else if (recordsRequested = 10) {
+        //     recordsRequested = 0;
+        // }
+
+        console.log("num records selected: " + recordsRequested);
+        console.log(searchTerm);
+
+
+        event.preventDefault();
+        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        url += '?' + $.param({
+            'api-key': "df04ac9d5b75418da22f348bc63aba52",
+            'q': searchTerm, //search
+            'begin_date': startYear, //begin date
+            'end_date': endYear //end date
+
         });
-    });
-    }
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function(result) {
+            // console.log(url);
+            console.log(result);
+            for (i = 0; i < recordsRequested; i++) {
+                console.log(result.response.docs[i].lead_paragraph);
+                console.log("num records selected: " + recordsRequested);
+                //write to page
+                $("#wellSection").append("<div>" + (i + 1) + "</div>");
+                $("#wellSection").append(result.response.docs[i].lead_paragraph);
+                $("#wellSection").append("<div>" + result.response.docs[i].byline.original + "</div>");
+                $("#wellSection").append("<div></div>");
+            }
+        }).fail(function(err) {
+            throw err;
+        });
+    };
+
+    $("#Search").on("click", queryData);
+});
